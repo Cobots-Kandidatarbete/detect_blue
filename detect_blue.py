058946f761b5d7898d5875fc5e12fa468b63b4c5
SamuelRosen95 <98107009+SamuelRosen95@users.noreply.github.com>
2022-03-09 16:00:18 +0100

    D435 OK

--- detect_blue.py
+++ detect_blue.py
@@ -2,23 +2,51 @@ import cv2
 import numpy as np
 import imutils
 
+"""
+import SimpleCV
+from SimpleCV import Camera
+import time
 
-cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cam = Camera()
+time.sleep(3)
+
+img = cam.getImage()
+
+"""
+
+cam = cv2.VideoCapture(2) #cv2.CAP_DSHOW)
+
+
+
+contador = 0 
+while True:
+    check, img = cam.read()
+    #cv2.imshow("Captura", frame)
+    key = cv2.waitKey(1)
+    if contador==10:                     #Low contador means low light
+        cv2.imwrite(filename='image.png', img=img)
+        break
+    contador=contador+1
+    #print(contador)
+cam.release()
+
+
   
 # reading the input using the camera
-result, img = cam.read()
-if not result:
-   raise ValueError("Cam not found")
+#result, img = cam.read()
+#if not result:
+   #raise ValueError("Cam not found")
 
-"""
-img_org = cv2.imread("lada.jpg")
-img = cv2.resize(img_org, (600,800))
-"""
+
+
+#img_org = cv2.imread("lada.jpg")
+#img = cv2.resize(img_org, (600,800))
+
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-lower_range = np.array([110,50,50])
-upper_range = np.array([130,255,255])
+lower_range = np.array([100,128,0])
+upper_range = np.array([215,255,255])
 
 mask = cv2.inRange(hsv, lower_range, upper_range)
 
